0c07ebd97442e2c385fd0bc6f6e47862f3ac1e87
Cecilia <98435962+Cecimartinez@users.noreply.github.com>
2023-08-27 04:46:39 -0300

    update margin sidebar

--- src/components/SideBar/SideBarMenu/SideBarMenu.jsx
+++ src/components/SideBar/SideBarMenu/SideBarMenu.jsx
@@ -3,7 +3,7 @@ import { ListItem } from "../../ListItems/ListItem";
 export const SideBarMenu = ({ open, mode, handleChange }) => {
   return (
     <div className="menu-bar my-5 py-6">
-      <ul className="menu ">
+      <ul className="menu  my-auto">
         <ListItem stateOpen={open} />
       </ul>
 
